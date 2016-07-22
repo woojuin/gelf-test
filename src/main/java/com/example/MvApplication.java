be1c038bde30b46e80238cc2d9bb75dac02851a3
woojuin <woojuin@neowiz.com>
2016-07-22 14:30:01 +0900

    asd

--- src/main/java/com/example/MvApplication.java
+++ src/main/java/com/example/MvApplication.java
@@ -1,8 +1,16 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class MvApplication {
 
