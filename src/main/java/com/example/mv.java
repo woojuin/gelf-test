6559658bfb219a7afcc3e6fbc93e79d333c503c5
woojuin <woojuin@neowiz.com>
2016-07-22 10:58:50 +0900

    n

---
+++ src/main/java/com/example/mv.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class mv {
+  @RequestMapping("/")
+  @ResponseBody
+  public String home() {
+    return "hello OPENSHIFT";
+  }
+}
