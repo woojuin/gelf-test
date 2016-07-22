2774cc9ce923461e94e784786d36b2438b230974
woojuin <woojuin@neowiz.com>
2016-07-22 11:51:27 +0900

    a

--- src/main/java/com/example/mv.java
+++ src/main/java/com/example/mv.java
@@ -1,14 +1,11 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class mv {
-  @RequestMapping("/openshift")
-  @ResponseBody
+  @RequestMapping("openshift")
   public String home() {
     return "hello OPENSHIFT";
   }
