f34f0ead9dd825cde4eea0e817b08d3f29fc6409
woojuin <vstarmanv@gmail.com>
2016-08-30 11:39:51 +0900

    ppush
    ppush

--- src/main/java/com/example/mv.java
+++ src/main/java/com/example/mv.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class mv {
   @RequestMapping("openshift")
   public String home() {
-    return ("hello OPENSHIFT WEBHOOK PUSH");
+    return ("hello OPENSHIFT WEBHOOK PUSH TOUR");
   }
 }
